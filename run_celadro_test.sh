4cdaf6ea891f0772acdc694eededaf7ec5589cf6
Lasse Bonn <rsx187@astro01.cluster>
2025-02-17 17:50:18 +0100

    minor

--- run_celadro_test.sh
+++ run_celadro_test.sh
@@ -5,7 +5,7 @@
 #SBATCH --mem=50G
 #SBATCH --account=astro
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=56
+#SBATCH --cpus-per-task=28
 export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
-srun ./build/celadro examples/bend/bend.dat -o test5e-7 -t 56
+srun ./build/celadro examples/bend_lowlowvolnoS/bend.dat -o /lustre/astro/rsx187/celadrodata/lowlowvolnoStest -t 28
